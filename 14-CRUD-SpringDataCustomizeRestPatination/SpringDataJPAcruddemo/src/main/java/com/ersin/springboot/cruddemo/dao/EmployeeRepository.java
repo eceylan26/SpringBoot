9db295cbac55a849a4cbd6e9bd2941686f9af808
Ersin CEYLAN <ecyln26@gmail.com>
2020-05-01 17:57:12 +0300

    14-CRUD-Spring-Data-Customize-Rest-Patination
    14-CRUD-Spring-Data-Customize-Rest-Patination

---
+++ 14-CRUD-SpringDataCustomizeRestPatination/SpringDataJPAcruddemo/src/main/java/com/ersin/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.ersin.springboot.cruddemo.dao;
+
+import com.ersin.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+
+@RepositoryRestResource(path = "members")
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+    // ne need any method
+
+}
